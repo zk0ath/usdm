e942fc9db0c218c91f92b34f27e4839bd8db932b
kadir alan <kadir.alan@prodyum.com>
2023-11-02 22:41:13 +0300

    add mint vue component to mint the coin

---
+++ ui/components/Mint.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input v-model="minter" placeholder="Minter Public Key" />
+    <input v-model="amount" placeholder="Amount" type="number" />
+    <button @click="mint">Mint</button>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      minter: '',
+      amount: 0,
+      message: '',
+    };
+  },
+  methods: {
+    async mint() {
+      const result = await someApiCallToMint(this.minter, this.amount); //TO-DO
+      this.message = result.message;
+    },
+  },
+};
+</script>
