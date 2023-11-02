6e7145ea87a996541a71273362695960eec447cb
kadir alan <kadir.alan@prodyum.com>
2023-11-02 22:16:38 +0300

    add burn.vue component to burn coins

---
+++ ui/components/Burn.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <input v-model="burner" placeholder="Burner Public Key" />
+      <input v-model="amount" placeholder="Amount to Burn" type="number" />
+      <button @click="burn">Burn</button>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    data() {
+      return {
+        burner: '',
+        amount: 0,
+        message: '',
+      };
+    },
+    methods: {
+      async burn() {
+        const result = await someApiCallToBurn(this.burner, this.amount); //TO-DO
+        this.message = result.message;
+      },
+    },
+  };
+  </script>
+  
