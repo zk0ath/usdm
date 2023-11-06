9316c7456868d0145e2119d1b50cc96f0e260bef
kadir alan <kadir.alan@prodyum.com>
2023-11-06 15:17:48 +0300

    Deploy gh-pages

---
+++ ui/out/_next/static/chunks/pages/_app-9df2fcc583cc739c.js
@@ -0,0 +1,73 @@
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([[888],{
+
+/***/ 5492:
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+
+    (window.__NEXT_P = window.__NEXT_P || []).push([
+      "/_app",
+      function () {
+        return __webpack_require__(3877);
+      }
+    ]);
+    if(false) {}
+  
+
+/***/ }),
+
+/***/ 3877:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ App; }
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5893);
+/* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2352);
+/* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_globals_css__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _reactCOIServiceWorker__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(3530);
+/* harmony import */ var _reactCOIServiceWorker__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_reactCOIServiceWorker__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+function App(param) {
+    let { Component, pageProps } = param;
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(Component, {
+        ...pageProps
+    });
+}
+
+
+/***/ }),
+
+/***/ 3530:
+/***/ (function() {
+
+function loadCOIServiceWorker() {
+    if ( true && window.location.hostname != "localhost") {
+        const coi = window.document.createElement("script");
+        coi.setAttribute("src", "/usdm/coi-serviceworker.min.js"); // update if your repo name changes for npm run deploy to work successfully
+        window.document.head.appendChild(coi);
+    }
+}
+loadCOIServiceWorker();
+
+
+/***/ }),
+
+/***/ 2352:
+/***/ (function() {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, [774,179], function() { return __webpack_exec__(5492), __webpack_exec__(9974); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
