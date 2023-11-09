76069d853dbf39a3e8c7282a3e92cc7449e3aa00
kadir alan <kadir.alan@prodyum.com>
2023-11-10 01:46:10 +0300

    implement receipenntadress for burn

--- ui/src/components/BurnForm.tsx
+++ ui/src/components/BurnForm.tsx
@@ -1,36 +1,58 @@
-import { Dispatch, SetStateAction } from 'react';
+import React from 'react';
 
 type BurnFormProps = {
   burnAmount: number;
-  setBurnAmount: Dispatch<SetStateAction<number>>;
+  setBurnAmount: React.Dispatch<React.SetStateAction<number>>;
+  burnRecipientAddress: string; // Added property for the recipient's address
+  setBurnRecipientAddress: React.Dispatch<React.SetStateAction<string>>; // Added setter for the recipient's address
   onBurnTokens: () => void;
 };
 
 const BurnForm: React.FC<BurnFormProps> = ({
   burnAmount,
   setBurnAmount,
+  burnRecipientAddress,
+  setBurnRecipientAddress,
   onBurnTokens,
 }) => {
   return (
-    <div className="flex flex-col space-y-2 items-center">
-      <input
-        type="number"
-        value={burnAmount}
-        onChange={(e) => setBurnAmount(Number(e.target.value))}
-        className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
-                  focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                  disabled:bg-gray-100 disabled:text-gray-500 disabled:border-gray-200 disabled:shadow-none
-                  invalid:border-pink-500 invalid:text-pink-600
-                  focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-        min="0" // Assuming burnAmount can't be negative
-      />
+    <section className="flex flex-col items-center p-4 bg-gray-50 rounded-lg shadow space-y-3">
+      <h2 className="text-lg font-semibold text-gray-700">Burn Tokens</h2>
+      <div className="w-full">
+        <label htmlFor="burnAmount" className="sr-only">Amount to Burn</label>
+        <input
+          id="burnAmount"
+          type="number"
+          value={burnAmount}
+          onChange={(e) => setBurnAmount(Number(e.target.value))}
+          className="w-full p-2 text-sm text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm placeholder-gray-400 
+                    focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 
+                    disabled:bg-gray-100 disabled:text-gray-500 disabled:border-gray-200 
+                    invalid:border-red-500 invalid:text-red-600 
+                    focus:invalid:border-red-500 focus:invalid:ring-red-500"
+          min="0"
+        />
+      </div>
+      <div className="w-full">
+        <label htmlFor="burnRecipientAddress" className="sr-only">Address</label>
+        <input
+          id="burnRecipientAddress"
+          type="text"
+          value={burnRecipientAddress}
+          onChange={(e) => setBurnRecipientAddress(e.target.value)}
+          className="w-full p-2 text-sm text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm placeholder-gray-400 
+                    focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 
+                    disabled:bg-gray-100 disabled:text-gray-500 disabled:border-gray-200"
+          placeholder="Recipient's Address"
+        />
+      </div>
       <button
         onClick={onBurnTokens}
-        className="px-4 py-2 bg-blue-600 text-white text-sm font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+        className="w-full px-5 py-3 text-sm font-medium text-white transition-colors duration-150 bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none"
       >
         Burn Tokens
       </button>
-    </div>
+    </section>
   );
 };
 
