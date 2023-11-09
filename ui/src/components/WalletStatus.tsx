203405a7aafe82ffa436c31ace6613256b4d46ef
kadir alan <kadir.alan@prodyum.com>
2023-11-10 01:45:46 +0300

    Change WalletStatus to toast message

--- ui/src/components/WalletStatus.tsx
+++ ui/src/components/WalletStatus.tsx
@@ -1,29 +1,26 @@
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
+
+
 type WalletStatusProps = {
     hasWallet: boolean;
     publicKey: string;
   };
   
   const WalletStatus = ({ hasWallet, publicKey }: WalletStatusProps) => {
-    if (!hasWallet) {
-      const auroLink = 'https://www.aurowallet.com/';
-      return (
-        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-          <strong className="font-bold">Wallet Not Detected! </strong>
-          <span className="block sm:inline">Could not find a wallet. </span>
-          <a href={auroLink} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 underline">
-            Install Auro wallet here
-          </a>.
-        </div>
-      );
-    }
+    useEffect(() => {
+      if (!hasWallet) {
+        toast.error("Wallet Not Detected! Could not find a wallet.", {
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      } else {
+        toast.success(`Wallet Connected! Public Key: ${publicKey}`, {
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      }
+    }, [hasWallet, publicKey]);
   
-    // Optionally render the publicKey if wallet is found and you wish to display it
-    return (
-      <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-        <strong className="font-bold">Wallet Connected! </strong>
-        <span className="block sm:inline">Public Key: {publicKey}</span>
-      </div>
-    ) || null;
+    return null; // No need to return any JSX, as the toast handles the display
   };
   
   export default WalletStatus;
