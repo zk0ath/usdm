a1b201323969816b2eb9d2e550be31e17252ee34
kadir alan <kadir.alan@prodyum.com>
2023-11-12 22:21:51 +0300

    remove cors headers

--- ui/next.config.js
+++ ui/next.config.js
@@ -21,14 +21,6 @@ const nextConfig = {
       {
         source: '/(.*)',
         headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
         ],
       },
     ];
