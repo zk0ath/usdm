17016f29ff8bfb91c700112c25dc3aebc150a58e
kadir alan <kadir.alan@prodyum.com>
2023-11-12 22:24:59 +0300

    remove headers

--- ui/next.config.js
+++ ui/next.config.js
@@ -16,15 +16,6 @@ const nextConfig = {
   },
   // To enable o1js for the web, we must set the COOP and COEP headers.
   // See here for more information: https://docs.minaprotocol.com/zkapps/how-to-write-a-zkapp-ui#enabling-coop-and-coep-headers
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-        ],
-      },
-    ];
-  },
   images: {
     unoptimized: true,
   },
