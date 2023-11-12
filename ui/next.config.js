4a138c681b704a963832b80ecb674f70d537d05a
kadir alan <kadir.alan@prodyum.com>
2023-11-12 23:00:25 +0300

    Revert "remove cors headers"
    This reverts commit a1b201323969816b2eb9d2e550be31e17252ee34.

--- ui/next.config.js
+++ ui/next.config.js
@@ -21,6 +21,14 @@ const nextConfig = {
       {
         source: '/(.*)',
         headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
         ],
       },
     ];
