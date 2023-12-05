ed4be35dcab8d508f400098107270d6d2c97d7ba
kadir alan <kadir.alan@prodyum.com>
2023-12-06 01:48:10 +0300

    Cover the project using redux provider and implement store

--- ui/src/pages/_app.page.tsx
+++ ui/src/pages/_app.page.tsx
@@ -1,16 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '@/components/Layout';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "@/components/Layout";
+import { Provider } from "react-redux";
+import store from "../store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import './reactCOIServiceWorker';
+import "./reactCOIServiceWorker";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <ToastContainer position="bottom-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <Component {...pageProps} />
+      </Provider>
     </Layout>
   );
 }
