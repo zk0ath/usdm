2869b659b291ac1e3029c8714edf8a285722bc24
Taha Zeybek <58766235+zybk23@users.noreply.github.com>
2023-12-06 23:33:41 +0300

    Added navigation bar which include connect wallet
    Added material-ui package  using components and features  for  coding fast.

---
+++ ui/src/components/InformationDialog.tsx
@@ -0,0 +1,63 @@
+import { Close } from "@mui/icons-material";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  IconButton,
+} from "@mui/material";
+
+// interface DialogProps {
+//     handleConfirmationAction?: () => void;
+//     handlePassiveAction?: () => void;
+//     processConfirmationMessage?: string;
+//     confirmButtonText?: string;
+//     rejectButtonText?: string;
+//     infoButtonText?: string;
+//     maxWidth?: any;
+//     confirmDeleteButtonText?: string;
+//     children?: React.ReactChild;
+// }
+
+const InformationDialog = ({ name }: any) => {
+  // const dispatch = useAppDispatch();
+  const isInformationDialogOpen = false;
+  const handleCloseDeleteDialog = () => {
+    // dispatch(setIsInformationDialogOpen(false));
+  };
+  return (
+    <>
+      <Dialog
+        maxWidth={"xs"}
+        sx={{ borderRadius: "24px" }}
+        onClose={handleCloseDeleteDialog}
+        fullWidth
+        aria-labelledby="customized-dialog-title"
+        open={isInformationDialogOpen}
+      >
+        <DialogContent dividers>
+          <IconButton
+            onClick={handleCloseDeleteDialog}
+            sx={{
+              top: 15,
+              right: 15,
+              position: "absolute",
+              bgcolor: "#181818",
+              width: { xs: 22, sm: 40 },
+              height: { xs: 22, sm: 40 },
+            }}
+          >
+            <Close sx={{ color: "#A0A0A0", fontSize: { xs: 14, sm: 22 } }} />
+          </IconButton>
+          <div className="flex flex-col items-center gap-4 mt-10">taha</div>
+        </DialogContent>
+        <DialogActions>
+          <div className="flex w-full  items-center justify-center py-4">
+            zeybek
+          </div>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default InformationDialog;
--- ui/src/pages/_app.page.tsx
+++ ui/src/pages/_app.page.tsx
@@ -4,6 +4,7 @@ import Layout from "@/components/Layout";
 import { Provider } from "react-redux";
 import store from "../store";
 import { ToastContainer } from "react-toastify";
+import Navigation from "@/components/Navigation";
 import "react-toastify/dist/ReactToastify.css";
 
 import "./reactCOIServiceWorker";
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Provider store={store}>
+        <Navigation />
         <ToastContainer
           position="bottom-right"
           autoClose={5000}
