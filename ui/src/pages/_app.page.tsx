c8af5ab1c247f197957a2f8d74f147d4fb5ce672
Taha Zeybek <58766235+zybk23@users.noreply.github.com>
2023-12-11 22:54:47 +0300

    The 'usedapp/core' package is implemented and covers the project for connecting to wallets
    index page regenerated for transactions proccess

--- ui/src/pages/_app.page.tsx
+++ ui/src/pages/_app.page.tsx
@@ -6,26 +6,48 @@ import store from "../store";
 import { ToastContainer } from "react-toastify";
 import Navigation from "@/components/Navigation";
 import "react-toastify/dist/ReactToastify.css";
-
+import {
+  Mainnet,
+  MetamaskConnector,
+  CoinbaseWalletConnector,
+  Config,
+  DAppProvider,
+} from "@usedapp/core";
+import { WalletConnectConnector } from "@usedapp/wallet-connect-connector";
 import "./reactCOIServiceWorker";
+
+const config: Config = {
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {},
+  connectors: {
+    metamask: new MetamaskConnector(),
+    coinbase: new CoinbaseWalletConnector(),
+    walletConnect: new WalletConnectConnector({
+      infuraId: "d8df2cb7844e4a54ab0a782f608749dd",
+    }),
+    // portis: new PortisConnector(PORTIS_DAPP_ID, 'mainnet'),
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Provider store={store}>
-        <Navigation />
-        <ToastContainer
-          position="bottom-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-        <Component {...pageProps} />
+        <DAppProvider config={config}>
+          <Navigation />
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+          <Component {...pageProps} />
+        </DAppProvider>
       </Provider>
     </Layout>
   );
