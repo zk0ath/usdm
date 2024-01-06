7d845bf57aa9e6a90b016863b7352718382936fa
inspector44 <kadiralan021@gmail.com>
2024-01-06 11:29:12 +0000

    upgrade burn method to use new version of o1js and eliminate signature usage

--- contracts/src/TokenContract.ts
+++ contracts/src/TokenContract.ts
@@ -15,10 +15,10 @@ import {
 } from 'o1js';
 
 export class TokenContract extends SmartContract {
-  @state(Field) symbol = State<Field>()
-  @state(UInt64) decimals = State<UInt64>()
-  @state(UInt64) maxSupply = State<UInt64>()
-  @state(PublicKey) owner = State<PublicKey>()
+  @state(Field) symbol = State<Field>();
+  @state(UInt64) decimals = State<UInt64>();
+  @state(UInt64) maxSupply = State<UInt64>();
+  @state(PublicKey) owner = State<PublicKey>();
   @state(UInt64) totalAmountInCirculation = State<UInt64>();
 
   deploy(args?: DeployArgs) {
@@ -30,62 +30,50 @@ export class TokenContract extends SmartContract {
     });
   }
 
-  @method initialize(symbol: Field, decimals: UInt64, maxSupply: UInt64){
-    this.symbol.set(symbol)
-    this.decimals.set(decimals)
-    this.maxSupply.set(maxSupply)
-    this.owner.set(this.sender)
+  @method initialize(symbol: Field, decimals: UInt64, maxSupply: UInt64) {
+    this.symbol.set(symbol);
+    this.decimals.set(decimals);
+    this.maxSupply.set(maxSupply);
+    this.owner.set(this.sender);
   }
 
   onlyOwner() {
-    this.owner.getAndRequireEquals().assertEquals(this.sender)
+    this.owner.getAndRequireEquals().assertEquals(this.sender);
   }
 
-  @method mint(
-    receiverAddress: PublicKey,
+  @method mint(receiverAddress: PublicKey, amount: UInt64) {
+    this.onlyOwner();
+    const maxSupply = this.maxSupply.getAndRequireEquals();
+    const circulatingSupply =
+      this.totalAmountInCirculation.getAndRequireEquals();
+
+    const newCirculatingSupply = circulatingSupply.add(amount);
+
+    newCirculatingSupply.assertLessThanOrEqual(maxSupply);
+
+    this.token.mint({
+      address: receiverAddress,
+      amount,
+    });
+
+    this.totalAmountInCirculation.set(newCirculatingSupply);
+  }
+
+  @method burn(
+    burnerAddress: PublicKey,
     amount: UInt64
   ) {
-    this.onlyOwner();
-    const maxSupply = this.maxSupply.getAndRequireEquals()
-        const circulatingSupply = this.totalAmountInCirculation.getAndRequireEquals()
+    // this.onlyOwner(); TODO: Question to caner: Should we keep this?
+    const circulatingSupply = this.totalAmountInCirculation.getAndRequireEquals()
 
-        const newCirculatingSupply = circulatingSupply.add(amount)
+        const newCirculatingSupply = circulatingSupply.sub(amount)
 
-        newCirculatingSupply.assertLessThanOrEqual(maxSupply)
-
-        this.token.mint({
-            address: receiverAddress,
+        this.token.burn({
+            address: burnerAddress,
             amount,
         })
 
         this.totalAmountInCirculation.set(newCirculatingSupply)
-  }
-
-  @method burn(
-    senderAddress: PublicKey,
-    amount: UInt64,
-    adminSignature: Signature
-  ) {
-    this.onlyOwner();
-    this.totalAmountInCirculation.assertEquals(
-      this.totalAmountInCirculation.get()
-    );
-    let totalAmountInCirculation = this.totalAmountInCirculation.get();
-
-    adminSignature
-      .verify(
-        this.address,
-        amount.toFields().concat(...senderAddress.toFields())
-      )
-      .assertTrue('Admin signature is invalid');
-
-    this.token.burn({
-      address: senderAddress,
-      amount,
-    });
-
-    let newTotalAmountInCirculation = totalAmountInCirculation.sub(amount);
-    this.totalAmountInCirculation.set(newTotalAmountInCirculation);
   }
 
   @method transfer(
