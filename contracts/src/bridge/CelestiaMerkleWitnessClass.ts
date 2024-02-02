108a1db8fdaf2a0ef1856b387b4cbe216f1d3bf3
inspector44 <kadiralan021@gmail.com>
2024-02-02 08:01:55 +0300

    missing celestia witness

---
+++ contracts/src/bridge/CelestiaMerkleWitnessClass.ts
@@ -0,0 +1,8 @@
+import { MerkleTree, MerkleWitness } from "o1js";
+
+const celestiaEmptyTree = new MerkleTree(21);
+export class CelestiaMerkleWitnessClass extends MerkleWitness(21) {
+    static empty(): CelestiaMerkleWitnessClass {
+      return new CelestiaMerkleWitnessClass(celestiaEmptyTree.getWitness(0n));
+    };
+  };
