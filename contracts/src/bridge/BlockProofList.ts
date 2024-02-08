c053b6a80a67e3bbbbe70b74e87fdfb44b45c79e
inspector44 <kadiralan021@gmail.com>
2024-02-08 05:01:31 +0300

    block proof list

--- contracts/src/bridge/BlockProofList.ts
+++ contracts/src/bridge/BlockProofList.ts
@@ -1,21 +1,21 @@
-import { Struct } from "o1js";
+import { PublicKey, Struct } from "o1js";
 import { BlockProof } from "./BlockProof";
 
 export class BlockProofList extends Struct({
-    proofs: Array.from({ length: MAX_SIGNER_COUNT }, () => BlockProof),
-    signers: Array.from({ length: MAX_SIGNER_COUNT }, () => PublicKey) // A signer public key array to keep track if the corresponding BlockProof Signer has already signed or not.
+    proofs: Array.from({ length: 200 }, () => BlockProof),
+    signers: Array.from({ length: 200 }, () => PublicKey) // A signer public key array to keep track if the corresponding BlockProof Signer has already signed or not.
   }) {
     constructor(
       proofs: BlockProof[]
     ) {
-      const signers = Array.from({ length: MAX_SIGNER_COUNT }, () => PublicKey.fromBase58(EMPTY_PUBLIC_KEY));
+      const signers = Array.from({ length: 200 }, () => PublicKey.fromBase58(EMPTY_PUBLIC_KEY));
   
       super({
         proofs,
         signers
       });
   
-      this.proofs = fillWithEmptyBlockProofs(proofs.splice(0, MAX_SIGNER_COUNT));
+      this.proofs = fillWithEmptyBlockProofs(proofs.splice(0, 200));
       this.signers = signers;
     };
   };
