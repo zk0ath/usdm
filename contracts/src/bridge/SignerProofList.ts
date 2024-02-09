529f9b9f056e1539e06d0c37d61f3a037cd33e33
inspector44 <kadiralan021@gmail.com>
2024-02-09 05:01:31 +0300

    add signer proof

---
+++ contracts/src/bridge/SignerProofList.ts
@@ -0,0 +1,34 @@
+import { PublicKey, Struct } from "o1js";
+import { SignerProof } from "./SignerProof";
+
+// SignerProofList holds an array of Proofs with length MAX_SIGNER_COUNT
+export class SignerProofList extends Struct({
+    proofs: Array.from({ length: 200 }, () => SignerProof),
+    signers: Array.from({ length: 200 }, () => PublicKey) // A signer public key array to keep track if the corresponding SignerProof Signer has already signed or not.
+  }) {
+    constructor(
+      proofs: SignerProof[]
+    ) {
+      const signers = Array.from({ length: 200 }, () => PublicKey.fromBase58(""));
+  
+      super({
+        proofs,
+        signers
+      });
+  
+      this.proofs = fillWithEmptySignerProofs(proofs.splice(0, 200));
+      this.signers = signers;
+    };
+  };
+
+
+  // A utility function to fill the given BlockProof array with empty SignerProofs until it reaches MAX_SIGNER_COUNT.
+function fillWithEmptySignerProofs(
+    [...proofs]: SignerProof[]
+  ): SignerProof[]{
+    const emptyProof = SignerProof.empty();
+  
+    for (let i = proofs.length; i < 200; i++)
+      proofs[i] = emptyProof;
+    return proofs;
+  };
