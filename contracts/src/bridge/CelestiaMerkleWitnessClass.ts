1014184b777e4675ad0eb380dbce6ad3ea65e460
inspector44 <kadiralan021@gmail.com>
2024-04-03 08:12:13 +0000

    checks

--- contracts/src/bridge/CelestiaMerkleWitnessClass.ts
+++
@@ -1,8 +0,0 @@
-import { MerkleTree, MerkleWitness } from "o1js";
-
-const celestiaEmptyTree = new MerkleTree(21);
-export class CelestiaMerkleWitnessClass extends MerkleWitness(21) {
-    static empty(): CelestiaMerkleWitnessClass {
-      return new CelestiaMerkleWitnessClass(celestiaEmptyTree.getWitness(0n));
-    };
-  };
--- contracts/src/bridge/SignerMerkleWitnessClass.ts
+++
@@ -1,8 +0,0 @@
-import { MerkleTree, MerkleWitness } from "o1js";
-
-const signerEmptyTree = new MerkleTree(20);
-export class SignerMerkleWitnessClass extends MerkleWitness(20) {
-    static empty(): SignerMerkleWitnessClass {
-      return new SignerMerkleWitnessClass(signerEmptyTree.getWitness(0n));
-    };
-  };
