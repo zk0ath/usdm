c7f403bab90a6ce8f6322945400351384ce8ae0e
inspector44 <kadiralan021@gmail.com>
2024-04-05 08:14:13 +0000

    fix the proof

--- contracts/src/SignerProof.ts
+++ contracts/src/SignerProof.ts
@@ -19,11 +19,29 @@ export class SignerProof extends Struct({
       this.signer = signer;
       this.signedSignerPublicKey = signedSignerPublicKey;
     };
-
+  
     static empty(): SignerProof {
       return new SignerProof(
         Signer.empty(),
         Signature.fromBase58(EMPTY_SIGNATURE)
+      );
+    };
+
+    isEmpty(): Bool {
+      return this.signer.isEmpty();
+    };
+  
+    verify(
+      signerRoot: Field,
+      signer: Signer,
+    ): Bool {
+      return this.signer.check(
+        signerRoot
+      ).and(
+        this.signedSignerPublicKey.verify(
+          this.signer.key,
+          [signer.hash()]
+        )
       );
     };
   };
