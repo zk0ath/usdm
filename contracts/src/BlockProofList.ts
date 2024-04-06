f5b35b0abeacd0ec3b114d18f7c935969657b77e
inspector44 <kadiralan021@gmail.com>
2024-04-06 08:13:13 +0000

    proof list implementation

---
+++ contracts/src/BlockProofList.ts
@@ -0,0 +1,21 @@
+import { Struct } from "o1js";
+import { BlockProof } from "./BlockProof";
+
+export class BlockProofList extends Struct({
+    proofs: Array.from({ length: MAX_SIGNER_COUNT }, () => BlockProof),
+    signers: Array.from({ length: MAX_SIGNER_COUNT }, () => PublicKey) // A signer public key array to keep track if the corresponding BlockProof Signer has already signed or not.
+  }) {
+    constructor(
+      proofs: BlockProof[]
+    ) {
+      const signers = Array.from({ length: MAX_SIGNER_COUNT }, () => PublicKey.fromBase58(EMPTY_PUBLIC_KEY));
+
+      super({
+        proofs,
+        signers
+      });
+
+      this.proofs = fillWithEmptyBlockProofs(proofs.splice(0, MAX_SIGNER_COUNT));
+      this.signers = signers;
+    };
+  };
