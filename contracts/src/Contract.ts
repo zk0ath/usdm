5db1be9255d084f98edd65fcc4238bd7f074d988
inspector44 <kadiralan021@gmail.com>
2024-04-07 09:14:13 +0000

    initialize the conditions for the contract

--- contracts/src/Contract.ts
+++ contracts/src/Contract.ts
@@ -1,4 +1,4 @@
-import { Field, Reducer, SmartContract, State, state } from "o1js";
+import { Field, Reducer, SmartContract, State, method, state } from "o1js";
 import { Signer } from "./Signer";
 
 export class BridgeContract extends SmartContract {
@@ -7,9 +7,9 @@ export class BridgeContract extends SmartContract {
     @state(Field) signerCount = State<Field>();
     @state(Field) signersTree = State<Field>();
     @state(Field) signersTreeAccumulator = State<Field>();
-
+  
     reducer = Reducer({ actionType: Signer });
-
+  
     init() {
       super.init();
       this.celestiaNamespaceHash.set(Field(0));
@@ -18,5 +18,22 @@ export class BridgeContract extends SmartContract {
       this.signersTree.set(Field(0));
       this.signersTreeAccumulator.set(Reducer.initialActionState);
     };
+
+    @method initialize(
+        celestiaNamespaceHash: Field,
+        initialCelestiaRootHash: Field,
+        initialSignerCount: Field,
+        initialSignerRootHash: Field
+      ) {
+        this.celestiaNamespaceHash.assertEquals(Field(0));
+        this.celestiaBlocksTree.assertEquals(Field(0));
+        this.signerCount.assertEquals(Field(0));
+        this.signersTree.assertEquals(Field(0));
+    
+        this.celestiaNamespaceHash.set(celestiaNamespaceHash);
+        this.celestiaBlocksTree.set(initialCelestiaRootHash);
+        this.signerCount.set(initialSignerCount);
+        this.signersTree.set(initialSignerRootHash);
+      };
 
   };
